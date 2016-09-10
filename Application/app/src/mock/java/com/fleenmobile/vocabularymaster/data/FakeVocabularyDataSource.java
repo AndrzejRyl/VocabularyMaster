c70ac8ebf0d2cc0ee6145341d9a7ff2df2f72c05
FleenMobile\Andrzej <fleenmobile@gmail.com>
2016-09-10 12:05:42 +0200

    Add mock flavor and data source (w/o implementation yet)

---
+++ Application/app/src/mock/java/com/fleenmobile/vocabularymaster/data/FakeVocabularyDataSource.java
@@ -0,0 +1,74 @@
+package com.fleenmobile.vocabularymaster.data;
+
+import android.support.annotation.NonNull;
+
+import com.fleenmobile.vocabularymaster.data.model.Stats;
+import com.fleenmobile.vocabularymaster.data.model.Vocabulary;
+import com.fleenmobile.vocabularymaster.data.source.VocabularyDataSource;
+
+import java.util.List;
+
+import rx.Observable;
+
+/**
+ * @author FleenMobile at 2016-09-10
+ */
+public class FakeVocabularyDataSource implements VocabularyDataSource {
+    @Override
+    public Observable<List<Vocabulary>> getVocabulary(int amount, int offset) {
+        // TODO
+        return null;
+    }
+
+    @Override
+    public Observable<List<Vocabulary>> getRandomVocabulary(int amount) {
+        // TODO
+        return null;
+    }
+
+    @Override
+    public Observable<List<Vocabulary>> getLearntVocabulary(int amount, int offset) {
+        // TODO
+        return null;
+    }
+
+    @Override
+    public Observable<List<Vocabulary>> getLearntVocabularySortedBy(int amount, int offset, @NonNull VocabularySortingStrategy sortedBy) {
+        // TODO
+        return null;
+    }
+
+    @Override
+    public Observable<Stats> getStats() {
+        // TODO
+        return null;
+    }
+
+    @Override
+    public Observable<Vocabulary> addVocabulary(@NonNull Vocabulary vocabulary) {
+        // TODO
+        return null;
+    }
+
+    @Override
+    public Observable<List<Vocabulary>> addVocabulary(@NonNull List<Vocabulary> vocabulary) {
+        // TODO
+        return null;
+    }
+
+    @Override
+    public Observable<List<Vocabulary>> removeVocabulary(@NonNull List<Vocabulary> vocabulary) {
+        // TODO
+        return null;
+    }
+
+    @Override
+    public void markVocabularyAsLearnt(List<Vocabulary> vocabulary) {
+        // TODO
+    }
+
+    @Override
+    public void updateVocabulary(Vocabulary vocabulary) {
+        // TODO
+    }
+}
