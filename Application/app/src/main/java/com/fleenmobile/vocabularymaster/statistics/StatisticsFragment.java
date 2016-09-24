a9c47e2965d98304e96386641484a8bd9056e3f4
FleenMobile\Andrzej <fleenmobile@gmail.com>
2016-09-24 13:13:15 +0200

    Add layout (w/o FAB popups)
    I didn't add expandable FABs (into popups) and I didn't write any adapters for data in words' lists.

--- Application/app/src/main/java/com/fleenmobile/vocabularymaster/statistics/StatisticsFragment.java
+++ Application/app/src/main/java/com/fleenmobile/vocabularymaster/statistics/StatisticsFragment.java
@@ -12,21 +12,24 @@ import com.fleenmobile.vocabularymaster.adding_words.AddFilePopupContract;
 import com.fleenmobile.vocabularymaster.adding_words.AddOneVocabularyPopupContract;
 import com.fleenmobile.vocabularymaster.data.model.Stats;
 import com.fleenmobile.vocabularymaster.data.model.Vocabulary;
+import com.github.aakira.expandablelayout.ExpandableRelativeLayout;
+import com.github.clans.fab.FloatingActionMenu;
 
 import java.util.List;
 
 import javax.inject.Inject;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
 /**
- *
  * This fragment contains three main parts
- *  - statistics about all learnt vocabulary (a list with translations)
- *  - statistics about top known vocabulary (a list with percentage of correct tries sorted by it desc)
- *  - statistics about worst known vocabulary (a list with percentage of correct tries sorted by it)
+ * - statistics about all learnt vocabulary (a list with translations)
+ * - statistics about top known vocabulary (a list with percentage of correct tries sorted by it desc)
+ * - statistics about worst known vocabulary (a list with percentage of correct tries sorted by it)
  *
  * @author FleenMobile at 2016-09-07
  */
@@ -38,6 +41,13 @@ public class StatisticsFragment extends Fragment implements StatisticsContract.V
     private boolean mBuyVocabularyPopupShown = false;
     private boolean mFABExpanded = false;
     private AddFilePopupContract.Presenter mAddFilePopupPresenter;
+
+    @BindView(R.id.stats_learnt_layout)
+    protected ExpandableRelativeLayout mainStatsLayout;
+    @BindView(R.id.fab_menu_overlay)
+    protected View fabMenuOverlay;
+    @BindView(R.id.fab_add_vocabulary)
+    protected FloatingActionMenu fabMenu;
 
     @Inject
     AddOneVocabularyPopupContract.View mAddOneVocabularyPopup;
@@ -60,6 +70,7 @@ public class StatisticsFragment extends Fragment implements StatisticsContract.V
                              Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.f_statistics, container, false);
         ButterKnife.bind(this, root);
+        fabMenu.setOnMenuButtonClickListener(this::onFabMenu);
         return root;
     }
 
@@ -142,5 +153,36 @@ public class StatisticsFragment extends Fragment implements StatisticsContract.V
     @Override
     public void setPresenter(StatisticsContract.Presenter presenter) {
         mPresenter = checkNotNull(presenter);
+    }
+
+    private void onFabMenu(View v) {
+        if (mFABExpanded) {
+            fabMenu.close(true);
+            fabMenuOverlay.setVisibility(View.GONE);
+        } else {
+            fabMenuOverlay.setVisibility(View.VISIBLE);
+            fabMenu.open(true);
+        }
+        mFABExpanded = !mFABExpanded;
+    }
+
+    @OnClick(R.id.fab_add_one_vocabulary)
+    public void onAddOneVocabulary(View v) {
+        showAddOneVocabularyPopup();
+    }
+
+    @OnClick(R.id.fab_add_file)
+    public void onAddFile(View v) {
+        showAddFilePopup();
+    }
+
+    @OnClick(R.id.fab_buy_vocabulary)
+    public void onBuyVocabulary(View v) {
+        showBuyVocabularyPopup();
+    }
+
+    @OnClick(R.id.stats_learnt_header)
+    public void onMainStatsHeader(View v) {
+        mainStatsLayout.toggle();
     }
 }
